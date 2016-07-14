ab20088d2c93c359106acb5698916b469f84a7d5
dmydry <dmitriy.gurinenko@gmail.com>
2016-07-14 16:26:38 +0800

    teamcity test order

--- src/tap-teamcity.js
+++ src/tap-teamcity.js
@@ -18,38 +18,40 @@ function tapTeamCity () {
   // Display current test started
   parser.on('test', (test) => {
     logger.finishTest()
-    logger.startTest(test)
-  })
+  logger.startTest(test)
+})
 
   // Display successful assertions started
-  parser.on('pass', (assertion) =>
-    logger.startAssertion(assertion))
+  parser.on('pass', (assertion) => {
+    logger.startAssertion(assertion);
+    logger.finishAssertion();
+})
 
-  // Display successful assertions finished
-  // Display failed assertions started
-  parser.on('assert', (assertion) => {
-    if (logger.assertion &&
-        logger.assertion.test === assertion.test &&
-        logger.assertion.number === assertion.number) {
-      logger.finishAssertion()
-    } else {
-      logger.startAssertion(assertion)
-    }
-  })
+  // // Display successful assertions finished
+  // // Display failed assertions started
+  // parser.on('assert', (assertion) => {
+  //   if (logger.assertion &&
+  //       logger.assertion.test === assertion.test &&
+  //       logger.assertion.number === assertion.number) {
+  //     logger.finishAssertion()
+  //   } else {
+  //     logger.startAssertion(assertion)
+  //   }
+  // })
 
   // Display failed assertions failed and finished
   parser.on('fail', (assertion) => {
     logger.failAssertion()
-    logger.finishAssertion()
-    stream.failed = true
-  })
+  logger.finishAssertion()
+  stream.failed = true
+})
 
   // Display everything finished
   parser.on('output', (results) => {
     logger.finishTest()
-    output.push('\n')
-    output.push(null)
-  })
+  output.push('\n')
+  output.push(null)
+})
 
   return stream
 }
